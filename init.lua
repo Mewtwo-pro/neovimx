0ecd5ae950322320f44184c13c8c6bfa39817d80
leoonpardo <BrentMillerIreland@outlook.com>
2026-06-16 15:20:13 -0500

    verjunio

---
+++ init.lua
@@ -0,0 +1,21 @@
+
+require ('basic')
+--require ('flashx')
+require ('linea_flash')
+require ('explorer')
+require ('autocomplete')
+require ('fzf_config')
+require ('auto_comando')
+require ('buscar')
+require ('git_net')
+require ('task')    
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-buffer'
+    use {
+    "ibhagwan/fzf-lua",
+    requires = { "nvim-tree/nvim-web-devicons" }
+    }
+end)
+
